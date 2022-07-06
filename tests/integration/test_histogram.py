c2faf4856eb67245d9c7b3fc5cf25f81ccd771f9
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2022-07-06 19:23:42 +0200

    Add test for along dimension

--- tests/integration/test_histogram.py
+++ tests/integration/test_histogram.py
@@ -21,7 +21,6 @@ class InternalError(Exception):
     pass
 
 
-
 np.random.seed(42)
 
 
@@ -109,3 +108,44 @@ def test_nd_flat(dim, kind, density, weight):
 
     # Check metadata
     assert h.shape == tuple(nbins)
+
+
+@pytest.mark.parametrize('kind', ['np', 'da'])
+@pytest.mark.parametrize('density', [True, False], ids=['densT', 'densF'])
+@pytest.mark.parametrize('weight', [True, False], ids=['wT', 'wF'])
+def test_1d_along(kind, density, weight):
+    nbins = 50
+    ranges = (-3, 3)
+    shape = (3, 200, 200)
+    chunk_size = (1, 200, 100)
+    vals = np.random.normal(size=shape).astype(np.float32)
+
+    if weight:
+        weights = np.random.uniform(size=shape).astype(np.float32)
+    else:
+        weights = None
+
+    if kind == 'np':
+        pass
+    elif kind == 'da':
+        vals = da.from_array(vals, chunks=chunk_size)
+        if weight:
+            weights = da.from_array(weights, chunks=chunk_size)
+    else:
+        raise InternalError
+
+    dims = ['t', 'x', 'y']
+    data = xr.DataArray(vals, dims=dims, name='data')
+    if weight:
+        weights = xr.DataArray(weights, dims=dims, name='weights')
+
+    bins = bh.axis.Regular(nbins, *ranges)
+    h = xh.histogram(data, bins=bins, density=density, weight=weights,
+                     dims=['x', 'y'])
+
+    # Check values
+    for i in range(shape[0]):
+        w = weights[i] if weight else None
+        answer, _ = np.histogram(vals[i], bins=nbins, range=ranges,
+                                 density=density, weights=w)
+        assert_allclose(answer, h.isel(t=i).values, rtol=1)
