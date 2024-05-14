064ac24627cdf38afa0f5c0b34167665007eae90
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2024-05-14 12:26:28 +0200

    Up docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,66 +1,89 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import xarray_histogram
 
-# -- Project information -----------------------------------------------------
+## Project information
 
-project = 'XArray-Histogram'
-copyright = '2022, Clément Haëck'
-author = 'Clément Haëck'
+project = "Xarray-Histogram"
+copyright = "2021, Clément Haëck"
+author = "Clément Haëck"
+
 release = xarray_histogram.__version__
+version = xarray_histogram.__version__
+print(f"xarray-histogram: {version}")
 
+## General configuration
 
-# -- General configuration ---------------------------------------------------
+templates_path = ["_templates"]
+# exclude_patterns = []
+pygments_style = "default"
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+nitpicky = True
+
 extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon'
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
 ]
 
+# Napoleon config
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
 napoleon_use_param = True
-napoleon_attr_annotations = True
-napoleon_use_ivar = True
-napoleon_preprocess_types = False
-always_document_param_types = True
+napoleon_use_rtype = False
+napoleon_preprocess_type = False
 
-autosummary_generate = False
+add_module_names = False
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# Autosummary config
+autosummary_generate = ["api.rst"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# Autodoc config
+autodoc_typehints = "description"
+autodoc_typehints_format = "short"
 
+# Intersphinx config
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
 
-# -- Options for HTML output -------------------------------------------------
+## HTML Output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_title = "Xarray-Histogram"
+html_theme_options = dict(
+    collapse_navigation=False,
+    use_download_button=True,
+    use_fullscreen_button=False,
+    # TOC
+    show_toc_level=2,
+    # Link to source in repo
+    repository_url="https://github.com/Descanonge/xarray-histogram",
+    use_source_button=True,
+    repository_branch="master",
+    path_to_docs="doc",
+    # Social icons
+    icon_links=[
+        dict(
+            name="Repository",
+            url="https://github.com/Descanonge/xarray-histogram",
+            icon="fa-brands fa-github",
+        ),
+        dict(
+            name="PyPI",
+            url="https://pypi.org/xarray-histogram",
+            icon="fa-brands fa-python",
+        ),
+    ],
+    # Footer
+    article_footer_items=["prev-next"],
+    content_footer_items=[],
+    footer_start=["footer-left"],
+    footer_end=["footer-right"],
+)
+html_last_updated_fmt = "%Y-%m-%d"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_sidebars = {"**": ["navbar-logo.html", "sbt-sidebar-nav.html", "icon-links.html"]}
