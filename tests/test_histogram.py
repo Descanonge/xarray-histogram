5825ad442f875b22ae0fbcb1a67d356273e2d4a4
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-03-07 16:15:35 +0100

    Move to utils

--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -6,8 +6,7 @@ Results are compared with those from np.histogram.
 Tested arrays are generated automatically. Numpy and Dask arrays are used
 """
 
-from collections.abc import Callable, Sequence
-from typing import Any
+from collections.abc import Sequence
 
 import boost_histogram as bh
 import dask.array as da
@@ -17,6 +16,8 @@ import xarray as xr
 from numpy.testing import assert_allclose
 
 import xarray_histogram as xh
+
+from .util import bool_param, get_array, get_ref_hist, get_weights, id_data, id_x
 
 np.random.seed(42)
 
@@ -57,61 +58,6 @@ class TestBinsInput:
             xh.core.get_axes_from_specs([5, 8], None, [None])
         with pytest.raises(IndexError):
             xh.core.get_axes_from_specs([5, 8], [(0.0, 10.0)], [None, None])
-
-
-def get_array(
-    shape: Sequence[int], chunks: Sequence[int] | None = None, name: str = "var1"
-) -> xr.DataArray:
-    if chunks is None:
-        data = np.random.rand(*shape)
-    else:
-        data = da.random.random(shape, chunks=chunks)
-
-    return xr.DataArray(data, dims=list("xywzuv"[: len(shape)]), name=name)
-
-
-def get_weights(x: xr.DataArray, weight: bool) -> xr.DataArray | None:
-    if not weight:
-        return None
-    chunks = [c[0] for c in x.chunks] if x.chunks else None
-    return get_array(x.shape, chunks=chunks, name="weights")
-
-
-def get_ref_hist(
-    *data: xr.DataArray,
-    axes: Sequence[bh.axis.Axis],
-    weights: xr.DataArray | None = None,
-    **kwargs,
-) -> np.ndarray:
-    # All inputs must have the same shape!!
-    args: Any = [x.compute().data.reshape(-1) for x in data]
-    kwargs["bins"] = [ax.edges for ax in axes]
-    kwargs["weights"] = (
-        weights.compute().data.reshape(-1) if weights is not None else None
-    )
-
-    if len(args) == 1:
-        func: Callable = np.histogram
-        args = args[0]
-        kwargs["bins"] = kwargs["bins"][0]
-    else:
-        func = np.histogramdd
-
-    ref, _ = func(args, **kwargs)
-    return ref
-
-
-def bool_param(name: str):
-    values = [True, False]
-    ids = [f"{name}:{v}" for v in values]
-    return pytest.mark.parametrize(name, values, ids=ids)
-
-
-def id_x(x: xr.DataArray):
-    s = f"x{x.shape}"
-    if x.chunks is not None:
-        s += str([d[0] for d in x.chunks])
-    return s
 
 
 class TestStorage:
@@ -346,10 +292,6 @@ class TestUnivariate:
         assert layers[0] == x.data.name
         assert layers[1].startswith("hist-on-block-")
         assert layers[2].startswith("sum-hist-aggregate-")
-
-
-def id_data(data: Sequence[xr.DataArray]) -> str:
-    return f"{{{':'.join(id_x(x) for x in data)}}}"
 
 
 class TestMultivariate:
