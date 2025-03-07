0f02bb0095a21b144c7df8993dce28aac34faa72
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2025-03-07 16:13:16 +0100

    Move tests

---
+++ tests/test_accessor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+class TestAccessibility:
+    pass
+
+
+class TestEdges:
+    # edges
+    # widths
+    # areas
+    pass
+
+
+class TestTransformBins:
+    pass
+
+
+class TestStatistics:
+    pass
